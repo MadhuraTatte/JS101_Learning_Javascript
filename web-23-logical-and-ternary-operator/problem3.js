d77ff56b721a82600104ae264a58787df6c710a4
Madhura Tatte <madhuratatte95@gmail.com>
2022-10-10 18:12:11 +0530

    logical and ternary operator

---
+++ web-23-logical-and-ternary-operator/problem3.js
@@ -0,0 +1,9 @@
+//Given and character if it is a consonant print "Consonant"
+
+let char="r";
+
+if(char=="a"|| char=="e"|| char=="i"|| char=="o"||char=="u"){
+  console.log("Vowel");
+}else {
+  console.log("Consonant");
+}
---
+++ web-23-logical-and-ternary-operator/problem4.js
@@ -0,0 +1,9 @@
+//Given 3 numbers (all different values), print which is greatest
+let a=25,b=62,c=12;
+if(a>b && a>c){
+  console.log(a,"is greater");
+}else if(b>a && b>c){
+  console.log(b,"is greater");
+}else{
+  console.log(c,"is greater");
+}
